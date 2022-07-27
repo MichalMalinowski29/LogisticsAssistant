6cca151a16f7256ccb88d711e750e9c8015c82d5
buzz <buzz@XCB-PC-TRAINEE1>
2022-07-27 10:01:26 +0200

    dodanie kolejnego modelu

---
+++ LogisticsAssistant/Models/Schedule.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LogisticsAssistant.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime departureStart { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime departureEnd { get; set; }
+        public decimal roadLength { get; set; }
+        public int truckId { get; set; }
+        public string? details { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime? scheduleCreatedAt { get; set; }
+    }
+}
