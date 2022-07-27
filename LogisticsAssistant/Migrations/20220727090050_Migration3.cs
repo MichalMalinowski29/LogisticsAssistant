ac2a39144ad74438e63dcc6f9c247074cd2db976
buzz <buzz@XCB-PC-TRAINEE1>
2022-07-27 11:01:34 +0200

    dodanie migracji

---
+++ LogisticsAssistant/Migrations/20220727090050_Migration3.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LogisticsAssistant.Migrations
+{
+    public partial class Migration3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Vmax",
+                table: "Truck",
+                newName: "vMax");
+
+            migrationBuilder.RenameColumn(
+                name: "BreakRequired",
+                table: "Truck",
+                newName: "breakRequired");
+
+            migrationBuilder.AddColumn<int>(
+                name: "truckId",
+                table: "Truck",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "truckId",
+                table: "Truck");
+
+            migrationBuilder.RenameColumn(
+                name: "vMax",
+                table: "Truck",
+                newName: "Vmax");
+
+            migrationBuilder.RenameColumn(
+                name: "breakRequired",
+                table: "Truck",
+                newName: "BreakRequired");
+        }
+    }
+}
